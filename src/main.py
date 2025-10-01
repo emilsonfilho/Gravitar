c5a8ed64fc63bdbeb33d891814cad3fab71ce7d3
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-10-01 19:25:37 -0300

    style: removing unecessary comments

--- src/main.py
+++ src/main.py
@@ -7,13 +7,6 @@ from utils.configLoader import ConfigLoader
 import numpy as np
 import sys
 
-
-"""
-sun = Body("Sol", 1.9885e30, np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 0.0]))
-earth = Body("Terra", 5.972e24, np.array([1.495e11, 0.0, 0.0]), np.array([0.0, 29.78e3, 0.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0,0.0,0.0]))
-moon = Body("Lua", 7.35e22, np.array([1.496e11, 3.844e8, 0.0]), np.array([1022.0, 29780.0, 0.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0,0.0,0.0]))
-"""
-
 if len(sys.argv) < 2:
     print("Error: You must provide a configuration file path as a command-line argument.")
     print("Usage: python3 main.py <path_to_your_config_file>")
