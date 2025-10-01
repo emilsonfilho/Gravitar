0ac29a07a62a9b49dda1403618440ff9946ff9fe
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-10-01 19:25:17 -0300

    style: removing unecessary comments

--- src/main.py
+++ src/main.py
@@ -14,38 +14,6 @@ earth = Body("Terra", 5.972e24, np.array([1.495e11, 0.0, 0.0]), np.array([0.0, 2
 moon = Body("Lua", 7.35e22, np.array([1.496e11, 3.844e8, 0.0]), np.array([1022.0, 29780.0, 0.0]), np.array([0.0, 0.0, 0.0]), np.array([0.0,0.0,0.0]))
 """
 
-
-# sun = Body(
-#     name="Corpo 1",
-#     mass=1,
-#     position=np.array([0.5773502691896258, 0.0, 0.0]),
-#     velocity=np.array([0.0, 0.7598356856515927, 0.0]),
-#     acceleration=np.array([0.0, 0.0, 0.0]),
-#     force=np.array([0.0, 0.0, 0.0])
-# )
-
-# # Um "Planeta" orbitando o Sol
-# earth = Body(
-#     name="Corpo 2",
-#     mass=1,
-#     position=np.array([-0.2886751345948128, 0.5, 0.0]), # Posição inicial
-#     velocity=np.array([-0.6580370064762464, -0.37991784282579616, 0.0]), # Velocidade para manter a órbita
-#     acceleration=np.array([0.0, 0.0, 0.0]),
-#     force=np.array([0.0, 0.0, 0.0])
-# )
-
-# # Uma "Lua" orbitando o Planeta
-# moon = Body(
-#     name="Corpo 3",
-#     mass=1,
-#     position=np.array([-0.2886751345948132, -0.5, 0.0]), # Posição inicial próxima ao planeta
-#     velocity=np.array([0.6580370064762461, -0.37991784282579666, 0.0]), # Velocidade do planeta + velocidade orbital
-#     acceleration=np.array([0.0, 0.0, 0.0]),
-#     force=np.array([0.0, 0.0, 0.0])
-# )
-
-
-
 if len(sys.argv) < 2:
     print("Error: You must provide a configuration file path as a command-line argument.")
     print("Usage: python3 main.py <path_to_your_config_file>")
