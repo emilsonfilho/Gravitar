662c642452e9b84415bf402a220e8d7a998a3783
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-10-02 12:14:01 -0300

    fix: visualization

--- src/utils/visualHandler.py
+++ src/utils/visualHandler.py
@@ -10,10 +10,10 @@ class VisualizationModeHandler:
         self.fps = fps
 
     def run(self):
-        if self.mode == VisualizationMode.PLOT:
+        if self.mode == VisualizationMode.PLOT.value:
             plt.show()
-        elif self.mode == VisualizationMode.VIDEO:
+        elif self.mode == VisualizationMode.VIDEO.value:
             self.anim.save(self.outFile, writer="ffmpeg", fps=self.fps)
-        elif self.mode == VisualizationMode.BOTH:
+        elif self.mode == VisualizationMode.BOTH.value:
             plt.show()
             self.anim.save(self.outFile, writer="ffmpeg", fps=self.fps)
